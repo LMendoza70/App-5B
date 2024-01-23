3a608be49d943281f8dec3110b62fd2c4b94d1b4
LMendoza70 <luis.mendoza@uthh.edu.mx>
2024-01-23 08:31:42 -0600

    23/01/24

--- App.js
+++ App.js
@@ -4,7 +4,18 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.encabezado}>
+        <Text style={styles.texto}>X</Text>
+        <Text style={styles.texto}>Nombre</Text>
+        <Text style={styles.texto}>X</Text>
+      </View>
+      <View style={styles.contenido}>
+        <Text>Proyecto de 5B</Text>
+      </View>
+      <View style={styles.pie}>
+        <Text>Pie de app</Text>
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +23,37 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 10,
+    backgroundColor: '#ffaa55',
     alignItems: 'center',
     justifyContent: 'center',
+    alignItems:'stretch'
   },
+  encabezado:{
+    flex:1,
+    flexDirection:'row',
+    borderColor:"#fff",
+    borderWidth:1,
+    justifyContent:'space-between',
+    alignItems:'center'
+    
+  },
+  pie:{
+    flex:1,
+    borderColor:"#fff",
+    borderWidth:1,
+
+    justifyContent:'center'
+  },
+  contenido:{
+    flex:8,
+    borderColor:"#fff",
+    borderWidth:1,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  texto:{
+    color:"#fff",
+    fontSize:30
+  }
 });
